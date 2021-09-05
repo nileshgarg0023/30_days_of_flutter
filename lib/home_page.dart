e01cb648d9df6cf440507ca0313e526a9fc36e22
nileshgarg0023 <nileshgarg0023@gmail.com>
2021-09-05 18:05:35 +0530

    day2 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("MentAR"),
+      ),
+      body: Center(
         child: Container(
           child: Text("Welcome to my first flutter app"),
         ),
       ),
+      drawer: Drawer(),
     );
   }
 }
